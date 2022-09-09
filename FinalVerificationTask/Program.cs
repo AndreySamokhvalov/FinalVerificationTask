7c053bde5eb024222240a30bafff416776392cfe
Andrey_Samokhvalov <_@example.com123321>
2022-09-10 01:23:16 +0300

    first commit

---
+++ FinalVerificationTask/Program.cs
@@ -0,0 +1,22 @@
+﻿// метод для формирования массива из введенных с клавиатуры строк
+string[] ReadData()
+{
+    Console.WriteLine("Задайте количество строк в массиве:");
+    int size = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте массив строк");
+    Console.Clear();
+    Console.WriteLine("Введите произвольные строки через Enter:");
+
+
+    string[] inputArray = new string[size];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        Console.Write((i + 1) + " строка " + ": ");
+        inputArray[i] = Console.ReadLine();
+        Console.Clear();
+    }
+    return inputArray;
+}
+
+
